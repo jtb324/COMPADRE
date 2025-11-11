506cad87fa18942daafe75710c6511cb541935ac
J.T. Baker <59581582+jtb324@users.noreply.github.com>
2025-11-10 20:32:28 -0600

    feature(main.py): - jtb324 - 11/10/25 - 8:32pm ---
    added a route that returns how many keys are in the database:
    ---
    Added a route called pair_count that returns how many keys are in the
    database as an integer. This route could be used for logging or to
    ensure the data was read in correctly

--- ibd_db/src/ibd_db/main.py
+++ ibd_db/src/ibd_db/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, Depends, HTTPException, Request, status
 import redis.asyncio as aredis
 import redis
+from redis.backoff import ExponentialBackoff
+from redis.retry import Retry
 from contextlib import asynccontextmanager
+
+
+retry = Retry(ExponentialBackoff(), 8)
 
 
 # The async context manager function as our connection pool that is made once for the lifespan of the application
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("initializing redis pool")
-    app.state.redis = await aredis.from_url("redis://localhost:6379")
+    app.state.redis = await aredis.from_url(
+        "redis://localhost:6379", decode_responses=True, retry=retry
+    )
     try:
         yield
     finally:
@@ -36,6 +43,20 @@ async def health_check(redis_conn: aredis.Redis = Depends(get_redis_connection))
 
         return {"status_code": status.HTTP_200_OK, "message": "Redis service is active"}
     except redis.exceptions.ConnectionError as e:
+        return {
+            "status_code": status.HTTP_503_SERVICE_UNAVAILABLE,
+            "message": "Unable to reach the redis server",
+        }
+
+
+@app.get("/pair_count/")
+async def pair_count(redis_conn: aredis.Redis = Depends(get_redis_connection)):
+
+    try:
+        dbsize = await redis_conn.dbsize()
+
+        return {"status_code": status.HTTP_200_OK, "message": dbsize}
+    except redis.exceptions.ConnectionError:
         return {
             "status_code": status.HTTP_503_SERVICE_UNAVAILABLE,
             "message": "Unable to reach the redis server",
